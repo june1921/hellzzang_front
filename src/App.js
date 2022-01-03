506c528fd13b359baefd7bd1501dba80e954af2b
JooHee Kim <winniekim0405@naver.com>
2022-01-03 19:01:55 +0900

    주히 월욜 커밋

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 /* eslint-disable */
-import { Navbar, Container, Nav, Row, Card } from "react-bootstrap";
+import { Navbar, Container, Nav, Row, Card, Button } from "react-bootstrap";
 import Col from 'react-bootstrap/Col';
 import './App.css';
 import React, { useState } from "react";
 import image from "./images.jpg";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import MyPage from "./pages/MyPage";
+//import { Route } from "react-router";
+
 
 function App() {
 
@@ -11,61 +15,69 @@ function App() {
   let [스위치, 스위치변경] = useState(false);
 
   return (
-    
+
     <div className="App">
       <Navbar expand="lg">
         <Container>
-          <Navbar.Brand href="/">로 고</Navbar.Brand>
+          <Navbar.Brand href="/">미션 임파서블!!</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              사 용 자 : <a href="#login">침착맨</a>
+              사 용 자 : <a href="/mypage">침착맨</a>
             </Navbar.Text>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
+
+
       <Nav variant="tabs" defaultActiveKey="link-0">
         <Nav.Item>
-          <Nav.Link eventKey="link-0" onClick={() => { 스위치변경(false); setPushTab(0); }}>메인</Nav.Link>
+          <Nav.Link eventKey="link-0" onClick={() => { 스위치변경(false); setPushTab(0); }}>Main</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-1" onClick={() => { 스위치변경(false); setPushTab(1); }}>명전</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2" onClick={() => { 스위치변경(false); setPushTab(2); }}>미정</Nav.Link>
+          <Nav.Link eventKey="link-1" onClick={() => { 스위치변경(false); setPushTab(1); }}>Rank</Nav.Link>
         </Nav.Item>
       </Nav>
-      <body>
-        <TabContent pushTab={pushTab} />
-      </body>
+
+
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route element={<TabContent pushTab={pushTab} />} path='/' />
+          <Route element={<MyPage />} path='/mypage' />
+        </Routes>
+      </BrowserRouter>
     </div>
-    
+
   );
 }
 
+
 function TabContent(props) {
   if (props.pushTab === 0) {
-    return <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 6 }).map((_, idx) => (
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={image} />
-            <Card.Body>
-              <Card.Title>카드 제목</Card.Title>
-              <Card.Text>
-                여기는 카드의 내용이 들어갈 부분이라고 생각이 드는곳임
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title>큰 미션</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Button variant="primary">user_id</Button>
+        </Card.Body>
+      </Card>
+    )
+    
   } else if (props.pushTab === 1) {
     return <h1>명예의 전당 들어갈곳</h1>
-  } else if (props.pushTab === 2) {
-    return <h1>여기에 들어갈건 아직 미정</h1>
+
   }
+
 }
+
+
 
 export default App;
