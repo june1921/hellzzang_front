37816c5d4c4c2b69bf14751b9560d2b7f332088b
chyeondori <chyeondori@gmail.com>
2022-01-03 20:35:02 +0900

    merge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { Navbar, Container, Nav, Row, Card, Button } from "react-bootstrap";
+import { Navbar, Container, Nav, Row, Card } from "react-bootstrap";
 import Col from 'react-bootstrap/Col';
 import './App.css';
 import React, { useState } from "react";
@@ -8,16 +8,9 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/WritePage';
 import PostPage from './pages/PostPage';
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes , Link} from "react-router-dom";
 
 import image from "./images.jpg";
-
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import MyPage from "./pages/MyPage";
-//import { Route } from "react-router";
-
-import Modal from "./components/Modal/Modal";
-
 
 function App() {
 
@@ -29,96 +22,57 @@ function App() {
     <div className="App">
       <Navbar expand="lg">
         <Container>
-          <Navbar.Brand href="/">미션 임파서블!!</Navbar.Brand>
+          <Navbar.Brand href="/">로 고</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-<<<<<<< HEAD
               사 용 자 : <a href="/login" onClick={() => { <LoginPage /> }}>침착맨</a>
-=======
-              사 용 자 : <a href="/mypage">침착맨</a>
->>>>>>> origin/master
             </Navbar.Text>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-
-
       <Nav variant="tabs" defaultActiveKey="link-0">
         <Nav.Item>
-          <Nav.Link eventKey="link-0" onClick={() => { 스위치변경(false); setPushTab(0); }}>Main</Nav.Link>
+          <Nav.Link eventKey="link-0" onClick={() => { 스위치변경(false); setPushTab(0); }}>메인</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-1" onClick={() => { 스위치변경(false); setPushTab(1); }}>Rank</Nav.Link>
+          <Nav.Link eventKey="link-1" onClick={() => { 스위치변경(false); setPushTab(1); }}>명전</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2" onClick={() => { 스위치변경(false); setPushTab(2); }}>미정</Nav.Link>
         </Nav.Item>
       </Nav>
-<<<<<<< HEAD
       {/* <TabContent pushTab={pushTab} /> */}
 
       <h1>로그인 창</h1>
       <BrowserRouter>
-
         <Routes>
-
-          <Route element = { <PostListPage /> } path='/:username' />
-          <Route element = { <LoginPage /> } path='/login'/>
-          <Route element = { <RegisterPage /> } path='/register' />
-          <Route element = { <WritePage /> } path='/write' />
-          <Route element = { <PostPage /> } path="/:username/:postId"/>
+          <Route element={<PostListPage />} path='/' />
+          <Route element={<LoginPage />} path="/login" />
+          <Route element={<RegisterPage />} path="/register" />
+          <Route element={<WritePage />} path="/write" />
+          <Route element={<PostPage />} path="/:username/:postId" />
+          {/* <Route render={() => {return <h1>Test</h1>} } path={['/@:username', '/']} exact/>
+          <Route component = { PostListPage } path={['/@:username', '/']} exact/>
+          <Route component = { LoginPage } path="/login"/>
+          <Route component = { RegisterPage } path="/register"/>
+          <Route component = { WritePage } path="/write"/>
+          <Route component = { PostPage } path="//@:username/:postId"/> */}
 
         </Routes>
-
-      </BrowserRouter>
-
+        </BrowserRouter>
 
     </div>
 
-=======
-
-
-
-
-      <BrowserRouter>
-        <Routes>
-          <Route element={<TabContent pushTab={pushTab} />} path='/' />
-          <Route element={<MyPage />} path='/mypage' />
-        </Routes>
-      </BrowserRouter>
-    </div>
->>>>>>> origin/master
   );
 }
 
-
 function TabContent(props) {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const openModal = () => {
-    setModalOpen(true);
-  };
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-
-
-
   if (props.pushTab === 0) {
-
-
     return <Row xs={1} md={2} className="g-4">
       {Array.from({ length: 6 }).map((_, idx) => (
-<<<<<<< HEAD
         <Col key={idx}>
-=======
-
-        <Col>
-          <React.Fragment>
-            <Modal open={modalOpen} close={closeModal} header="Modal heading">
-            </Modal>
-          </React.Fragment>
-          <a onClick= {openModal}>
->>>>>>> origin/master
           <Card>
             <Card.Img variant="top" src={image} />
             <Card.Body>
@@ -128,19 +82,14 @@ function TabContent(props) {
               </Card.Text>
             </Card.Body>
           </Card>
-          </a>
         </Col>
       ))}
     </Row>
-
-
   } else if (props.pushTab === 1) {
     return <h1>명예의 전당 들어갈곳</h1>
-
+  } else if (props.pushTab === 2) {
+    return <h1>여기에 들어갈건 아직 미정</h1>
   }
-
 }
-
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const store = createStore (rootReducer, composeWithDevTools());
 ReactDOM.render(
   <Provider store = {store}>
   <React.StrictMode>
-   
+
     <App />
 
   </React.StrictMode>,
